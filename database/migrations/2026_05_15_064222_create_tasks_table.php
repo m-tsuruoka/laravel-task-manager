effc482c3203ef948789f2a32b4ab5dac633d292
m.tsuruoka <m.tsuruoka@core-tech.jp>
2026-05-18 14:52:58 +0900

    期限管理機能の実装

--- database/migrations/2026_05_15_064222_create_tasks_table.php
+++ database/migrations/2026_05_15_064222_create_tasks_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
     $table->string('title', 100);
     $table->text('description')->nullable();
     $table->tinyInteger('status')->default(0);
+    $table->date('due_date')->nullable();
     $table->foreignId('user_id')->constrained()->onDelete('cascade');
     $table->timestamps();
 });
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -23,6 +23,14 @@
         name="description"
         value="{{ old('description', $task->description)}}"
     ><br>
+    <label>ステータス</label>
+<select name="status">
+    <option value="0" {{ $task->status == 0 ? 'selected' : '' }}>未着手</option>
+    <option value="1" {{ $task->status == 1 ? 'selected' : '' }}>進行中</option>
+    <option value="2" {{ $task->status == 2 ? 'selected' : '' }}>完了</option>
+</select>
+    <label>日時</label>
+            <input  value="{{ old('due_date', $task->due_date)}}" type="date" name="due_date">
 
     <button type="submit">更新</button>
 </form>
